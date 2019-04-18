e1142c990a32237dd287bdd9310c8ca4637e623d
FajarM <muhammadfajarm@gmail.com>
2019-04-19 00:04:38 +0700

    commit v2

---
+++ app/src/main/java/com/morningstar/lat2_akb11_10116490/AlmostThereActivity.java
@@ -0,0 +1,30 @@
+package com.morningstar.lat2_akb11_10116490;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+/*
+ * Created Bye
+ *
+ * NIM     : 10116490
+ * Nama    : Muhammad Fajar Maulana
+ * Kelas   : IF-11 / AKB-11
+ * Tanggal : 18-April-2019
+ *
+ * */
+public class AlmostThereActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_almost_there);
+
+        View overlay = findViewById(R.id.loggin);
+
+        overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+}
--- app/src/main/java/com/morningstar/lat2_akb11_10116490/RegisterActivity.java
+++ app/src/main/java/com/morningstar/lat2_akb11_10116490/RegisterActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.TextView;
 /*
  * Created Bye
  *
@@ -19,29 +18,30 @@ import android.widget.TextView;
  * Tanggal : 18-April-2019
  *
  * */
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    private Button register;
-
+    private Button btnregister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
-        View overlay = findViewById(R.id.loggin);
+        View overlay = findViewById(R.id.reggister);
 
         overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
 
-        register = (Button) findViewById(R.id.btnRegister);
-        register.setOnClickListener(new View.OnClickListener() {
+        btnregister = (Button) findViewById(R.id.btn_Register);
+        btnregister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent moveIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                Intent moveIntent = new Intent(RegisterActivity.this, AlmostThereActivity.class);
                 startActivity(moveIntent);
             }
         });
     }
+
+
 }
